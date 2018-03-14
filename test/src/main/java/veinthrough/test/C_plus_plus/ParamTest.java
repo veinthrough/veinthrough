50aea0b5cfcc7fb3f36c1fe793fc7ddfb9a7a602
veinthrough <veinthrough@gmail.com>
2018-03-14 12:40:50 +0800

    Optimization

--- test/src/main/java/veinthrough/test/C_plus_plus/ParamTest.java
+++ test/src/main/java/veinthrough/test/C_plus_plus/ParamTest.java
@@ -1,7 +1,11 @@
+/**
+ *
+ */
 package veinthrough.test.C_plus_plus;
 
+import veinthrough.test.AbstractUnitTester;
+
 /**
- * @version 1.00 2000-01-27
  * @author veinthrough
  * <p>
  * This program demonstrates parameter passing in Java, contains:
@@ -11,88 +15,99 @@ package veinthrough.test.C_plus_plus;
  * 2. Methods can change the state of object parameters.
  * <p>
  * 3. Methods can't attach new objects to object parameters, that is, can't swap object parameters.
+ *
  */
-public class ParamTest
-{
-   public static void main(String[] args)
-   {
-      /*
-       * Test 1: Methods can't modify numeric parameters
-       */
-      System.out.println("Testing tripleValue:");
-      double percent = 10;
-      System.out.println("Before: percent=" + percent);
-      tripleValue(percent);
-      System.out.println("After: percent=" + percent);
+public class ParamTest extends AbstractUnitTester {
 
-      /*
-       * Test 2: Methods can change the state of object parameters
-       */
-      System.out.println("\nTesting tripleSalary:");
-      Employee harry = new Employee("Harry", 50000);
-      System.out.println("Before: salary=" + harry.getSalary());
-      tripleSalary(harry);
-      System.out.println("After: salary=" + harry.getSalary());
+    /* (non-Javadoc)
+     * @see veinthrough.test.UnitTester#test()
+     */
+    @Override
+    public void test() {
+        /*
+         * Test 1: Methods can't modify numeric parameters
+         */
+        System.out.println("Testing tripleValue:");
+        double percent = 10;
+        System.out.println("Before: percent=" + percent);
+        tripleValue(percent);
+        System.out.println("After: percent=" + percent);
 
-      /*
-       * Test 3: Methods can't attach new objects to object parameters
-       */
-      System.out.println("\nTesting swap:");
-      Employee a = new Employee("Alice", 70000);
-      Employee b = new Employee("Bob", 60000);
-      System.out.println("Before: a=" + a.getName());
-      System.out.println("Before: b=" + b.getName());
-      swap(a, b);
-      System.out.println("After: a=" + a.getName());
-      System.out.println("After: b=" + b.getName());
-   }
+        /*
+         * Test 2: Methods can change the state of object parameters
+         */
+        System.out.println("\nTesting tripleSalary:");
+        Employee harry = new Employee("Harry", 50000);
+        System.out.println("Before: salary=" + harry.getSalary());
+        tripleSalary(harry);
+        System.out.println("After: salary=" + harry.getSalary());
 
-   public static void tripleValue(double x) // doesn't work
-   {
-      x = 3 * x;
-      System.out.println("End of method: x=" + x);
-   }
+        /*
+         * Test 3: Methods can't attach new objects to object parameters, that is, can't swap object parameters.
+         */
+        System.out.println("\nTesting swap:");
+        Employee a = new Employee("Alice", 70000);
+        Employee b = new Employee("Bob", 60000);
+        System.out.println("Before: a=" + a.getName());
+        System.out.println("Before: b=" + b.getName());
+        swap(a, b);
+        System.out.println("After: a=" + a.getName());
+        System.out.println("After: b=" + b.getName());
+    }
 
-   public static void tripleSalary(Employee x) // works
-   {
-      x.raiseSalary(200);
-      System.out.println("End of method: salary=" + x.getSalary());
-   }
+    private static void tripleValue(double x) // doesn't work
+    {
+        x = 3 * x;
+        System.out.println("End of method: x=" + x);
+    }
 
-   public static void swap(Employee x, Employee y)
-   {
-      Employee temp = x;
-      x = y;
-      y = temp;
-      System.out.println("End of method: x=" + x.getName());
-      System.out.println("End of method: y=" + y.getName());
-   }
-}
+    private static void tripleSalary(Employee x) // works
+    {
+        x.raiseSalary(200);
+        System.out.println("End of method: salary=" + x.getSalary());
+    }
 
-class Employee // simplified Employee class
-{
-   private String name;
-   private double salary;
+    private static void swap(Employee x, Employee y)
+    {
+        Employee temp = x;
+        x = y;
+        y = temp;
+        System.out.println("End of method: x=" + x.getName());
+        System.out.println("End of method: y=" + y.getName());
+    }
 
-   public Employee(String n, double s)
-   {
-      name = n;
-      salary = s;
-   }
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        new ParamTest().test();
+    }
 
-   public String getName()
-   {
-      return name;
-   }
+    class Employee // simplified Employee class
+    {
+        private String name;
+        private double salary;
 
-   public double getSalary()
-   {
-      return salary;
-   }
+        public Employee(String n, double s)
+        {
+            name = n;
+            salary = s;
+        }
 
-   public void raiseSalary(double byPercent)
-   {
-      double raise = salary * byPercent / 100;
-      salary += raise;
-   }
+        public String getName()
+        {
+            return name;
+        }
+
+        public double getSalary()
+        {
+            return salary;
+        }
+
+        public void raiseSalary(double byPercent)
+        {
+            double raise = salary * byPercent / 100;
+            salary += raise;
+        }
+    }
 }
